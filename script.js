e45ff9a6c23f89c198da600e5306160d28073f2b
DooZeeR <zsolt.ordas@gmail.com>
2021-12-02 16:29:38 +0100

    kép már majdnem jó

--- script.js
+++ script.js
@@ -28,10 +28,19 @@ functionName(1,2);
 
 */
 
-const inputElement = (type, name, label) =>{
+
+
+
+
+
+
+
+
+
+const inputElement = (type, name, label, req="") =>{
     return `
-        <div id="${type}">
-        <input type="${type}" name="${name}" id="${name}" placeholder="${label}" title="${label}">
+        <div class="${type}">
+        <input type="${type}" name="${name}" id="${name}" placeholder="${label}" title="${label}" ${req}>
         <label for="${name}">${label}</label>
         </div>
     `;
@@ -48,7 +57,7 @@ const selectElement = (type, name, label, options) =>{
     }
 
     return `
-        <div id="${type}">
+        <div class="${type}">
         <${type} name="${name}" id="">
         ${optionsToSelect}
         </${type}>
@@ -57,48 +66,61 @@ const selectElement = (type, name, label, options) =>{
     `;
 };
 
-{/* <select name="cars" id="cars">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="mercedes">Mercedes</option>
-  <option value="audi">Audi</option>
-</select> */}
 
 
 
 
+const anotherFormFields = [
+    {
+        type: 'text',
+        name: 'street',
+        label: 'közterület neve'
+    },
+    {
+        type: 'number',
+        name: 'housenumber',
+        label: 'Házszám'
+    },
+    {
+        type: 'number',
+        name: 'zipCode',
+        label: 'Irányítószám'
+    },
+    {
+        type: 'text',
+        name: 'city',
+        label: 'Település neve'
+    }
+];
 
 
 
+const formFields = [
+    {type: "file", name: "profilePicture", label: "Profilképed"},
+    {type: "text", name: "firstName", label: "Keresztneved"},
+    {type: "email", name: "personalEmail", label: "Email címed"},
+    {type: "password", name: "password", label: "Jelszó"},
+    {type: "checkbox", name: "newsLetter", label: "Hírlevelet szeretnél-e kapni"},
+    {type: "checkbox", name: "terms", label: "Elfogadom a felhasználási feltételeket"}
+];
 
-
-
-
-/*
-const formElement = 
-"<form>" 
-+ inputElement('text', 'firstName') 
-+ ${inputElement('file', 'profilePicture')}
-+ ${inputElement('email', 'personalEmail')}
-+ ${inputElement('radio', 'newsLetter')}
-+ ${inputElement('checkbox', 'terms')}
-+ "</form>";
-*/
-
-
-const formElement = `
-    <form id="form">
-        <div id="reg"><h1>Registration</h1></div>
-        ${inputElement('file', 'profilePicture', 'Profilképed')}
-        ${inputElement('text', 'firstName', 'Keresztneved')}
-        ${inputElement('email', 'personalEmail', 'Email címed')}
-        ${inputElement('password', 'password', 'Jelszó')}
-        ${inputElement('checkbox', 'newsLetter', 'Hírlevelet szeretnél-e kapni')}
-        ${inputElement('checkbox', 'terms', 'Elfogadom a felhasználási feltételeket')}
+const formElement = (ffs, id) => {
+    let fieldsHTML =``;
+    for (const ff of ffs) {
+        fieldsHTML += inputElement(ff.type, ff.name, ff.label, ff.req);
+    }
+    return `
+    <form id=${id}>
+        <div class="reg"><h1>Registration</h1></div>
+        ${fieldsHTML}
         ${selectElement('select', 'where', 'Hol hallottál rólunk', ['interneten','ismerőstől','egyéb'])}
         <button id="btn">OK</button>
     </form>
-`;
+    `;
+};
+
+
+
 
 
 
@@ -116,12 +138,21 @@ const formSubmit = (event) => {
 
 
 
-/* const inputUpdate = (event) => {
-    if (event.target.name === 'firstName') {
+const inputUpdate = (event) => {
+    if (event.target.getAttribute("name") === 'firstName') {
         document.getElementById('inputValue').innerHTML = event.target.value;
     }
-    console.log(event.target.closest("#form"));
-}; */
+    if (event.target.getAttribute("name") === 'profilePicture') {
+        //document.getElementById('inputValue').innerHTML = event.target.value;
+        console.log(event.target.files[0]);
+
+        const image = URL.createObjectURL(event.target.files[0]);
+        document.querySelector('#form div.file').insertAdjacentHTML('afterbegin', `
+        <img src="${image}">
+        `);
+    }
+    //console.log(event.target.closest("#form"));
+};
 
 
 
@@ -131,19 +162,20 @@ const formSubmit = (event) => {
 
 function loadEvent() {
     const rootElement = document.getElementById('root');
-    rootElement.insertAdjacentHTML('afterbegin', formElement);
-    /* rootElement.insertAdjacentHTML('afterbegin', `
+    rootElement.insertAdjacentHTML('afterbegin', formElement(formFields, "form"));
+    //rootElement.insertAdjacentHTML('afterbegin', formElement(anotherFormFields, "form2"));
+     rootElement.insertAdjacentHTML('afterbegin', `
     <div id="inputValue"></div>
-    `); */
+    `); 
 
     const form = document.getElementById('form');
     form.addEventListener('submit', formSubmit);
 
 
-    /* const inputList = form.querySelectorAll('input');
+    const inputList = form.querySelectorAll('input');
     for (const input of inputList) {
         input.addEventListener('input', inputUpdate)
-    } */
+    } 
 }
   
 window.addEventListener("load", loadEvent);
@@ -157,19 +189,23 @@ window.addEventListener("load", loadEvent);
 
 
 /* 
-1. kellő távolság legyen a beviteli mezők között
-1. kellő távolság legyen a beviteli mezők között és a mezőkben
-2. elég nagy legyen az input mező
-3. label ne legyen egy sorban az inputtal     block típus
-4. label és az input össze legyen kötve (lást label for="")
-5. minden képernyőméreten ugyanúgy használható legyen
-6.rendezett legyen legyen egy vonalban
-7. lehessen tabolni (ha formban van benne akkor lehet)
-8. logikus sorrend legyen
-9. aktuális input mező kiemelkedjen
+1.  kellő távolság legyen a beviteli mezők között
+1.  kellő távolság legyen a beviteli mezők között és a mezőkben
+2.  elég nagy legyen az input mező
+3.  label ne legyen egy sorban az inputtal     block típus
+4.  label és az input össze legyen kötve (lást label for="")
+5.  minden képernyőméreten ugyanúgy használható legyen
+6.  rendezett legyen legyen egy vonalban
+7.  lehessen tabolni (ha formban van benne akkor lehet)
+8.  logikus sorrend legyen
+9.  aktuális input mező kiemelkedjen
 10. gomb legyen hangsúlyos
 11. legyen megn 
 valid
 required
 kontraszt
+
+
+FileReader.readAsDataURL()
+URL.createObjectURL()
 */
